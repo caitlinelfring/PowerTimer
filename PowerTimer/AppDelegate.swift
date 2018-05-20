e5c2f80a9e636bf12fc81398ea5a77ab71e58f8d
Caitlin Elfring <caitlin@graciouseloise.com>
2018-05-20 10:51:48 -0400

    s/navigationController/root/

--- PowerTimer/AppDelegate.swift
+++ PowerTimer/AppDelegate.swift
@@ -25,8 +25,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     self.window!.backgroundColor = UIColor(red: 0.789, green: 1, blue: 0.837, alpha: 1) // trying to match lauch screen
     self.window!.makeKeyAndVisible()
-    let navigationController = UINavigationController(rootViewController: TimerViewController())
-    self.window!.rootViewController = navigationController
+    let root = UINavigationController(rootViewController: TimerViewController())
+    self.window!.rootViewController = root
 
     // Animation based on https://github.com/okmr-d/App-Launching-like-Twitter
     // logo mask
@@ -35,32 +35,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
       mask.contents = UIImage(named: "clock_icon.png")!.cgImage
       mask.bounds = CGRect(x: 0, y: 0, width: 60, height: 60)
       mask.anchorPoint = CGPoint(x: 0.5, y: 0.5)
-      mask.position = CGPoint(x: navigationController.view.frame.width / 2, y: navigationController.view.frame.height / 2)
+      mask.position = CGPoint(x: root.view.frame.width / 2, y: root.view.frame.height / 2)
       return mask
     }()
-    navigationController.view.layer.mask = mask
-
+    root.view.layer.mask = mask
 
     // logo mask background view
-    let maskBgView = UIView(frame: navigationController.view.frame)
+    let maskBgView = UIView(frame: root.view.frame)
     maskBgView.backgroundColor = UIColor.black
-    navigationController.view.addSubview(maskBgView)
-    navigationController.view.bringSubview(toFront: maskBgView)
+    root.view.addSubview(maskBgView)
+    root.view.bringSubview(toFront: maskBgView)
 
     // logo mask animation
     let transformAnimation = CAKeyframeAnimation(keyPath: "bounds")
     transformAnimation.delegate = self
     transformAnimation.duration = 1
-    transformAnimation.beginTime = CACurrentMediaTime() + 1 //add delay of 1 second
-    let initalBounds = NSValue(cgRect: navigationController.view.layer.mask!.bounds)
-    let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 50, height: 50))
-    let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: 8000, height: 8000))
+    transformAnimation.beginTime = CACurrentMediaTime() + 1 // add delay of 1 second
+    let initalBounds = NSValue(cgRect: mask.bounds)
+    let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: mask.bounds.width - 10, height: mask.bounds.height - 10))
+    let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: mask.bounds.width * 300, height: mask.bounds.height * 300))
     transformAnimation.values = [initalBounds, secondBounds, finalBounds]
     transformAnimation.keyTimes = [0, 0.3, 1]
-    transformAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
+    transformAnimation.timingFunctions = [
+      CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut),
+      CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut),
+    ]
     transformAnimation.isRemovedOnCompletion = false
     transformAnimation.fillMode = kCAFillModeForwards
-    navigationController.view.layer.mask!.add(transformAnimation, forKey: "maskAnimation")
+    mask.add(transformAnimation, forKey: "maskAnimation")
 
     // logo mask background view animation
     UIView.animate(withDuration: 0.1, delay: 1.35, options: UIViewAnimationOptions.curveEaseIn, animations: {
@@ -95,22 +97,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
   }
 
-  // Based on https://github.com/rounak/TwitterBirdAnimation
-  func animate(mask: CALayer) {
-    let keyFrameAnimation = CAKeyframeAnimation(keyPath: "bounds")
-    keyFrameAnimation.delegate = self
-    keyFrameAnimation.duration = 1
-    keyFrameAnimation.beginTime = CACurrentMediaTime() + 1 //add delay of 1 second
-    let initalBounds = NSValue(cgRect: mask.bounds)
-    let secondBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: mask.bounds.width - 10, height: mask.bounds.height - 10))
-    let finalBounds = NSValue(cgRect: CGRect(x: 0, y: 0, width: mask.bounds.width * 200, height: mask.bounds.height * 200))
-    keyFrameAnimation.values = [initalBounds, secondBounds, finalBounds]
-    keyFrameAnimation.keyTimes = [0, 0.3, 1]
-    keyFrameAnimation.timingFunctions = [CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut), CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)]
-    mask.add(keyFrameAnimation, forKey: "bounds")
-  }
-
 }
+
 extension AppDelegate: CAAnimationDelegate {
   func animationDidStop(_ anim: CAAnimation, finished flag: Bool) {
     // FIXME: on my iPhone 6, the mask is shown again for a split second after the animation completes
