96cf0a20faaa165c6c0c93129cf14ebc3378590b
Caitlin Elfring <caitlin@graciouseloise.com>
2018-06-05 15:56:02 -0400

    Support ipad app icons

--- app_icons.sh
+++ app_icons.sh
@@ -7,10 +7,6 @@ OUTPUT_DIR=$2
 sips -Z 120 ${MASTER} --out ${OUTPUT_DIR}/Icon120.png
 sips -Z 80 ${MASTER} --out ${OUTPUT_DIR}/Icon80.png
 
-# iPhone: App - iOS 5,6 (57pt 1x,2x)
-sips -Z 114 ${MASTER} --out ${OUTPUT_DIR}/Icon114.png
-sips -Z 57 ${OUTPUT_DIR}/Icon114.png --out ${OUTPUT_DIR}/Icon57.png
-
 # iPhone: App - iOS 7,8 (60pt 2x,3x)
 sips -Z 180 ${MASTER} --out ${OUTPUT_DIR}/Icon180.png
 
@@ -20,3 +16,10 @@ sips -Z 60 ${MASTER} --out ${OUTPUT_DIR}/Icon60.png
 
 # app store (1024)
 sips -Z 1024 ${MASTER} --out ${OUTPUT_DIR}/Icon1024.png
+
+# ipad
+sips -Z 20 ${MASTER} --out ${OUTPUT_DIR}/Icon20.png
+sips -Z 29 ${MASTER} --out ${OUTPUT_DIR}/Icon29.png
+sips -Z 76 ${MASTER} --out ${OUTPUT_DIR}/Icon76.png
+sips -Z 152 ${MASTER} --out ${OUTPUT_DIR}/Icon152.png
+sips -Z 167 ${MASTER} --out ${OUTPUT_DIR}/Icon167.png
