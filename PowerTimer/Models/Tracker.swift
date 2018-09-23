dbde547f6e8dc4af56600a9e14bb1ba3ad233c0e
Caitlin Elfring <caitlin@graciouseloise.com>
2018-09-23 10:19:51 -0400

    Firebase events can only contain letters, numbers, underscores

--- PowerTimer/Models/Tracker.swift
+++ PowerTimer/Models/Tracker.swift
@@ -11,7 +11,8 @@ import Firebase
 
 class Tracking {
   class func log(_ event: String, parameters: [String: Any]? = nil) {
-    print("event tracking: \(event) with params: \(String(describing: parameters))")
-    Analytics.logEvent(event, parameters: parameters)
+    let evt = event.replacingOccurrences(of: ".", with: "_") // Firebase event name must contain only letters, numbers, or underscores
+    print("event tracking: \(evt) with params: \(String(describing: parameters))")
+    Analytics.logEvent(evt, parameters: parameters)
   }
 }
