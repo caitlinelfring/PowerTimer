1b71dd4df5feec55d03aa3a4948f2316ca1b6730
Caitlin Elfring <celfring@gmail.com>
2019-05-05 13:57:29 -0400

    Add close button to prepare countdown

--- PowerTimer/PrepareTimerViewController.swift
+++ PowerTimer/PrepareTimerViewController.swift
@@ -19,6 +19,7 @@ class PrepareTimerViewController: UIViewController {
   private let timer = PTTimer.Down(startSeconds: seconds)
 
   private let timerView = UILabel()
+  private let close = CloseButton()
 
   convenience init(completion: (() -> ())?) {
     self.init()
@@ -47,6 +48,12 @@ class PrepareTimerViewController: UIViewController {
       make.left.equalTo(self.view.safeAreaLayoutGuide.snp.left)
       make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right)
     }
+    self.view.addSubview(self.close)
+    self.close.snp.makeConstraints { (make) in
+      make.top.equalTo(self.view.safeAreaLayoutGuide.snp.top).inset(10)
+      make.right.equalTo(self.view.safeAreaLayoutGuide.snp.right).inset(10)
+    }
+    self.close.addTarget(self, action: #selector(self.closeTimer), for: .touchUpInside)
 
     self.timer.delegate = self
     self.timer.start()
@@ -55,6 +62,11 @@ class PrepareTimerViewController: UIViewController {
   override func viewDidLayoutSubviews() {
     super.viewDidLayoutSubviews()
     self.timerView.font = TimerView.Constants.Active.font.withSize(self.view.minSafeAreaDimension())
+    self.close.iconSize = self.view.minSafeAreaDimension() / 20
+  }
+
+  @objc func closeTimer() {
+    self.timer.pause()
   }
 }
 
@@ -66,5 +78,41 @@ extension PrepareTimerViewController: PTTimerDelegate {
 
   func timerDidPause() {
     self.dismiss(animated: true, completion: self.completion)
+  }
+}
+
+private class CloseButton: UIButton {
+
+  var iconSize: CGFloat = 10
+  var lineWidth: CGFloat = 1
+  var lineColor: UIColor = UIColor.white.withAlphaComponent(0.54)
+
+  convenience init(iconSize: CGFloat, lineWidth: CGFloat) {
+    self.init()
+    self.iconSize = iconSize
+    self.lineWidth = lineWidth
+  }
+
+  override func draw(_ rect: CGRect) {
+    let path = UIBezierPath()
+
+    path.lineWidth = lineWidth
+    path.lineCapStyle = .round
+
+    let iconFrame = CGRect(
+      x: (rect.width - iconSize) / 2.0,
+      y: (rect.height - iconSize) / 2.0,
+      width: iconSize,
+      height: iconSize
+    )
+
+    path.move(to: iconFrame.origin)
+    path.addLine(to: CGPoint(x: iconFrame.maxX, y: iconFrame.maxY))
+    path.move(to: CGPoint(x: iconFrame.maxX, y: iconFrame.minY))
+    path.addLine(to: CGPoint(x: iconFrame.minX, y: iconFrame.maxY))
+
+    lineColor.setStroke()
+
+    path.stroke()
   }
 }
